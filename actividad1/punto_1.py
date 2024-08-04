8c8da87c931c6fbec064f8c95c32b3bfa22cbd50
Alejandro Peña Restrepo <alejoperes2003@gmail.com>
2024-08-04 16:15:22 -0500

    commit punto 1

---
+++ actividad1/punto_1.py
@@ -0,0 +1,8 @@
+# Crear un programa que pida al usuario ingresar su nombre y edad y los imprima en pantalla.
+
+print("por favor ingresar los datos")
+nombre=input("ingrese nombre:")
+edad=int(input("ingrese edad:"))
+print("el nombre es", nombre,"\nla edad es:", edad)
+
+
