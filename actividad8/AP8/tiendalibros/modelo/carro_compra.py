ae09aeb844c7fc478dd2d8fb0bb6009e6a99af25
Alejandro Peña Restrepo <alejoperes2003@gmail.com>
2024-10-09 14:16:47 -0500

    punto 1,2,3 de actividad 8

--- actividad8/AP8/tiendalibros/modelo/carro_compra.py
+++ actividad8/AP8/tiendalibros/modelo/carro_compra.py
@@ -1,9 +1,3 @@
 class CarroCompras:
-    pass
-    # Defina metodo inicializador __init__(self)
-
-    # Defina el metodo agregar_item
-
-    # Defina el metodo calcular_total
-
-    # Defina el metodo quitar_item
+   def __init__(self,item)->[]:
+      self.item :list =item
--- actividad8/AP8/tiendalibros/modelo/item_compra.py
+++ actividad8/AP8/tiendalibros/modelo/item_compra.py
@@ -1,2 +1,10 @@
 class ItemCompra:
-    pass
+ def __init__(self,libro,cantidad_libro,int):
+  self.libro=libro
+  self.cantidad_libro=cantidad_libro
+  self.int=int
+
+def calcular_subtotal(self):
+ self.precio_libro=self.cantidad_libro*self.int
+
+ 
