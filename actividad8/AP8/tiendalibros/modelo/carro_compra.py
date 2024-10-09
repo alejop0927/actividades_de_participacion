9ac3e694cf0a0587d2c707da7c95e160a776b6da
Alejandro Peña Restrepo <alejoperes2003@gmail.com>
2024-10-09 14:35:56 -0500

    punto 4,5 de actividad 8

--- actividad8/AP8/tiendalibros/modelo/carro_compra.py
+++ actividad8/AP8/tiendalibros/modelo/carro_compra.py
@@ -1,3 +1,18 @@
+from item_compra import ItemCompra,Libro1
 class CarroCompras:
    def __init__(self,item)->[]:
       self.item :list =item
+
+   def agregar_item(self):
+      self.agreg=ItemCompra(Libro1)
+      self.cantidad_l=ItemCompra(self.cantidad)
+      self.item.append(self.agreg,self.cantidad_l)
+      return self.item
+
+   def calcular_total(self):
+      self.sumita=sumita
+      sumita=sum(ItemCompra(self.precio))
+      return sumita
+
+
+
--- actividad8/AP8/tiendalibros/modelo/item_compra.py
+++ actividad8/AP8/tiendalibros/modelo/item_compra.py
@@ -1,10 +1,11 @@
 class ItemCompra:
- def __init__(self,libro,cantidad_libro,int):
-  self.libro=libro
+ def __init__(self,libro,cantidad_libro,precio):
+  self.libro:str=libro
   self.cantidad_libro=cantidad_libro
-  self.int=int
+  self.precio=precio
 
 def calcular_subtotal(self):
  self.precio_libro=self.cantidad_libro*self.int
 
- 
+
+Libro1=ItemCompra('satanas',2,2.500)
