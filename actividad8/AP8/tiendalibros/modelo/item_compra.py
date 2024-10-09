0893cd725cf16669a8ee8520271937f95d3a0448
Alejandro Peña Restrepo <alejoperes2003@gmail.com>
2024-10-09 15:03:44 -0500

    punto 8 y 9 de actividad 8

--- actividad8/AP8/tiendalibros/modelo/item_compra.py
+++ actividad8/AP8/tiendalibros/modelo/item_compra.py
@@ -1,6 +1,7 @@
 class ItemCompra:
- def __init__(self,isbn,libro,cantidad_libro,precio):
+ def __init__(self,isbn,titulo,libro,cantidad_libro,precio):
   self.isbn=isbn
+  self.titulo=titulo
   self.libro:str=libro
   self.cantidad_libro=cantidad_libro
   self.precio=precio
@@ -9,4 +10,4 @@ def calcular_subtotal(self):
  self.precio_libro=self.cantidad_libro*self.int
 
 
-Libro1=ItemCompra('satanas',2,2.500)
+Libro1=ItemCompra('pepe',2,2.500)
--- actividad8/AP8/tiendalibros/modelo/tienda_libros.py
+++ actividad8/AP8/tiendalibros/modelo/tienda_libros.py
@@ -4,4 +4,8 @@ class TiendaLibros:
    def __init__(self):
       self.catalogo=dict(ItemCompra(self.isbn,self.precio))
       self.carrito=CarroCompras
+   def __str__(self):
+      ItemCompra(self.isbn) and ItemCompra(self.titulo) in self.carrito
+      print(f"el isbn {ItemCompra(self.isbn)} y el titulo {ItemCompra(self.titulo)} ya esta en el carro compra")
+      
       
